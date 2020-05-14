e339eabb17176cacf4137bfb24113b871b022a75
Richard Cho <96richardohc@gmail.com>
2020-05-14 07:04:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   val = nil
   key = nil
   name_hash.each do |name, value|
-    if value == nil || value < val
+    if val == nil || value < val
       val = value
       key = name 
     end
